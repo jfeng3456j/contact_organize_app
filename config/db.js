8ee8a5d3ad4ba5a3132ef3ac7b709d91c553eed3
jfeng3456j <jfeng3456j@gmail.com>
2021-11-14 23:33:03 -0600

    change mongodb connection to use aync n await instead of promise

--- config/db.js
+++ config/db.js
@@ -4,14 +4,16 @@ const config = require('config');
 //get all the config properties with config.get()
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-    mongoose.connect(db)
-        .then(() => console.log("Connected to MongoDB..."))
-        .catch(err => {
-            console.log(err.message);
-            //exit with failure
-            process.exit(1);
-        });
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db);
+        console.log("Connected to MongoDB...");
+    }
+    catch (err) {
+        console.log(err.message);
+        //exit with failure
+        process.exit(1);
+    }
 }
 
 module.exports = connectDB;
