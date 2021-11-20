c0f41b72add431e2fcdf637c61288c801a02447f
jfeng3456j <jfeng3456j@gmail.com>
2021-11-19 23:12:30 -0600

    add a mongo user schema, use expressvalidator to validate inputs, bcrypt, and jwt

--- config/db.js
+++ config/db.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+
 //access to config dir
-const config = require('config');
+// const config = require('config');
 //get all the config properties with config.get()
-const db = config.get('mongoURI');
+// const db = config.get('mongoURI');
+
+const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@contactkeeper.sklwj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db);
+        await mongoose.connect(process.env.URI || db);
         console.log("Connected to MongoDB...");
     }
     catch (err) {
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,85 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+//express validator import
+const { check, validationResult } = require("express-validator");
+//bcrypt import
+const bcrypt = require("bcryptjs");
+//JWT
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
+const config = require("config");
 
 //@route    POST api/users
 //@desc     Register a user
 //@access   Public
-router.post('/', (req, res) => {
-    res.send('Register a user');
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please Include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const error = validationResult(req);
+
+    if (!error.isEmpty()) {
+      return res.status(400).json({ errors: error.array() });
+    }
+
+    const { name, email, password } = req.body;
+    try {
+      //pass a mongo User Object to user
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      //create new user based on the destructure req.body
+      user = new User({
+        name,
+        email,
+        password,
+      });
+      //use bcrypt to add sale
+      const salt = await bcrypt.genSalt(10);
+
+      //hash the password
+      user.password = await bcrypt.hash(password, salt);
+
+      //call the mongo save sql
+      await user.save();
+
+      //create the payload
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      //jwt.io for jwt decoder
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          //3600 is an hour
+          expiresIn: 3600,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
